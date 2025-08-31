72850ae06d53c9d589f7bd50559c348d414ae4e5
manvinautiya87919 <manvinautiya87919@gmail.com>
2025-08-31 14:25:16 +0530

    Add files via upload

---
+++ main (12).c
@@ -0,0 +1,52 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+
+
+
+/*
+Sample Test Cases:
+Input 1:
+-5
+Output 1:
+Negative
+
+Input 2:
+0
+Output 2:
+Zero
+
+Input 3:
+10
+Output 3:
+Positive
+
+*/
+#include <stdio.h>
+
+int main() {
+    int num;
+
+    // Input number
+    printf("Enter an integer: ");
+    scanf("%d", &num);
+
+    // Check using nested if–else
+    if (num >= 0) {
+        if (num == 0) {
+            printf("The number is Zero.\n");
+        } else {
+            printf("The number is Positive.\n");
+        }
+    } else {
+        printf("The number is Negative.\n");
+    }
+
+    return 0;
+}
+
