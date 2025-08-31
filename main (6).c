d00efd9bd2f0414d71290a8d9257734d700be209
manvinautiya87919 <manvinautiya87919@gmail.com>
2025-08-31 13:43:48 +0530

    Add files via upload

---
+++ main (6).c
@@ -0,0 +1,41 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+/*
+Sample Test Cases:
+Input 1:
+3 5
+Output 1:
+After swap: 5 3
+
+Input 2:
+-1 1
+Output 2:
+After swap: 1 -1
+
+*/
+#include <stdio.h>
+
+int main() {
+    int a, b, temp;
+
+    // Input two numbers
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+
+    printf("Before swapping: a = %d, b = %d\n", a, b);
+
+    // Swapping using a third variable
+    temp = a;
+    a = b;
+    b = temp;
+
+    printf("After swapping: a = %d, b = %d\n", a, b);
+
+    return 0;
+}
