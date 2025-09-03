17d2917b3f7063a7a69baa96d4fc0596cf0dc7f2
manvinautiya87919 <manvinautiya87919@gmail.com>
2025-09-03 23:07:16 +0530

    Add files via upload

---
+++ main (21).c
@@ -0,0 +1,74 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
+  Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+
+
+/*
+Sample Test Cases:
+Input 1:
+2
+Output 1:
+February, 28 days
+
+Input 2:
+12
+Output 2:
+December, 31 days
+
+*/
+#include <stdio.h>
+
+int main() {
+    int month;
+
+    printf("Enter month number (1-12): ");
+    scanf("%d", &month);
+
+    switch (month) {
+        case 1:
+            printf("January - 31 days\n");
+            break;
+        case 2:
+            printf("February - 28 or 29 days\n");
+            break;
+        case 3:
+            printf("March - 31 days\n");
+            break;
+        case 4:
+            printf("April - 30 days\n");
+            break;
+        case 5:
+            printf("May - 31 days\n");
+            break;
+        case 6:
+            printf("June - 30 days\n");
+            break;
+        case 7:
+            printf("July - 31 days\n");
+            break;
+        case 8:
+            printf("August - 31 days\n");
+            break;
+        case 9:
+            printf("September - 30 days\n");
+            break;
+        case 10:
+            printf("October - 31 days\n");
+            break;
+        case 11:
+            printf("November - 30 days\n");
+            break;
+        case 12:
+            printf("December - 31 days\n");
+            break;
+        default:
+            printf("Invalid month number! Please enter 1-12.\n");
+    }
+
+    return 0;
+}
