244c9ced88df1c7d6368d3e0d6005b1d07eb396d
manvinautiya87919 <manvinautiya87919@gmail.com>
2025-09-03 23:28:51 +0530

    Add files via upload

---
+++ main (27).c
@@ -0,0 +1,40 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+
+
+/*
+Sample Test Cases:
+Input 1:
+3
+Output 1:
+9
+
+Input 2:
+5
+Output 2:
+25
+
+*/
+#include <stdio.h>
+
+int main() {
+    int n, i, sum = 0;
+
+    printf("Enter the value of n: ");
+    scanf("%d", &n);
+
+    for (i = 1; i <= n; i++) {
+        sum += (2 * i - 1); 
+    }
+
+    printf("Sum of first %d odd numbers = %d\n", n, sum);
+
+    return 0;
+}
+
