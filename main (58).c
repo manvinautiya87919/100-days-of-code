207fe79b4c25e62c28b4111bb52930f63a52c549
manvinautiya87919 <manvinautiya87919@gmail.com>
2025-09-18 20:34:43 +0530

    Add files via upload

---
+++ main (58).c
@@ -0,0 +1,58 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+
+
+/*
+Sample Test Cases:
+Input 1:
+5
+2 9 1 4 7
+Output 1:
+Max=9, Min=1
+
+Input 2:
+3
+10 10 10
+Output 2:
+Max=10, Min=10
+
+*/
+#include <stdio.h>
+
+int main() {
+    int n, i;
+
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    // Input elements
+    printf("Enter %d elements:\n", n);
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    
+    int max = arr[0], min = arr[0];
+
+    
+    for (i = 1; i < n; i++) {
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+        if (arr[i] < min) {
+            min = arr[i];
+        }
+    }
+
+    printf("Max=%d, Min=%d\n", max, min);
+
+    return 0;
+}
