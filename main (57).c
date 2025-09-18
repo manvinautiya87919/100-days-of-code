2359ffd5ce5d7569e6cdec34196c73e8df987d53
manvinautiya87919 <manvinautiya87919@gmail.com>
2025-09-18 20:32:45 +0530

    Add files via upload

---
+++ main (57).c
@@ -0,0 +1,47 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+
+
+/*
+Sample Test Cases:
+Input 1:
+4
+2 4 6 8
+Output 1:
+20
+
+Input 2:
+3
+1 1 1
+Output 2:
+3
+
+*/
+#include <stdio.h>
+
+int main() {
+    int n, i, sum = 0;
+
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    
+    printf("Enter %d elements:\n", n);
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+        sum += arr[i]; 
+    }
+
+    
+    printf("Sum of array elements = %d\n", sum);
+
+    return 0;
+}
