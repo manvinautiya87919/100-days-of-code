e458b362abb9948cbee9f0a906841e3d593a3143
manvinautiya87919 <manvinautiya87919@gmail.com>
2025-09-25 21:05:56 +0530

    Add files via upload

---
+++ main (69).c
@@ -0,0 +1,61 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+/*
+Sample Test Cases:
+Input 1:
+5
+10 20 30 40 50
+Output 1:
+40
+
+*/
+#include <stdio.h>
+
+int main() {
+    int arr[100], n, i;
+    int largest, secondLargest;
+
+    printf("Enter the number of elements:\n");
+    scanf("%d", &n);
+
+    if (n < 2) {
+        printf("Array must have at least two elements.\n");
+        return 1;
+    }
+
+    printf("Enter %d integers:\n", n);
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    if (arr[0] > arr[1]) {
+        largest = arr[0];
+        secondLargest = arr[1];
+    } else {
+        largest = arr[1];
+        secondLargest = arr[0];
+    }
+
+    for (i = 2; i < n; i++) {
+        if (arr[i] > largest) {
+            secondLargest = largest;
+            largest = arr[i];
+        } else if (arr[i] > secondLargest && arr[i] != largest) {
+            secondLargest = arr[i];
+        }
+    }
+
+    if (largest == secondLargest) {
+        printf("No distinct second largest element.\n");
+    } else {
+        printf("%d\n", secondLargest);
+    }
+
+    return 0;
+}
