ac52c3047336320c6995524167d86989b1794e43
manvinautiya87919 <manvinautiya87919@gmail.com>
2025-09-25 21:18:57 +0530

    Add files via upload

---
+++ main (71).c
@@ -0,0 +1,46 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+/*
+Sample Test Cases:
+Input 1:
+2 2
+1 2
+3 4
+Output 1:
+1 2
+3 4
+
+*/
+#include <stdio.h>
+
+int main() {
+    int matrix[100][100];
+    int rows, cols, i, j;
+
+    printf("Enter number of rows and columns:\n");
+    scanf("%d %d", &rows, &cols);
+
+    printf("Enter the matrix elements:\n");
+    for (i = 0; i < rows; i++) {
+        for (j = 0; j < cols; j++) {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    printf("Matrix is:\n");
+    for (i = 0; i < rows; i++) {
+        for (j = 0; j < cols; j++) {
+            printf("%d ", matrix[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
+
