4afea29a0a509d8ca29ac7fa00d2a27f0427f8b5
manvinautiya87919 <manvinautiya87919@gmail.com>
2025-09-25 20:46:28 +0530

    Add files via upload

---
+++ main (65).c
@@ -0,0 +1,63 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+/*
+Sample Test Cases:
+Input 1:
+5
+1 3 5 7 9
+7
+Output 1:
+Found at index 3
+
+Input 2:
+5
+1 3 5 7 9
+6
+Output 2:
+-1
+
+*/
+#include <stdio.h>
+
+int main() {
+    int arr[100], n, i, key;
+    int low, high, mid, found = 0;
+
+    printf("Enter the number of elements:\n");
+    scanf("%d", &n);
+
+    printf("Enter %d sorted integers:\n", n);
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Enter the element to search:\n");
+    scanf("%d", &key);
+
+    low = 0;
+    high = n - 1;
+    while (low <= high) {
+        mid = (low + high) / 2;
+        if (arr[mid] == key) {
+            printf("Found at index %d\n", mid);
+            found = 1;
+            break;
+        } else if (arr[mid] < key) {
+            low = mid + 1;
+        } else {
+            high = mid - 1;
+        }
+    }
+
+    if (!found) {
+        printf("-1\n");
+    }
+
+    return 0;
+}
