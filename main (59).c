51160bc31c2119f8aeb0883b7c498f9cc1f09538
manvinautiya87919 <manvinautiya87919@gmail.com>
2025-09-25 20:03:18 +0530

    Add files via upload

---
+++ main (59).c
@@ -0,0 +1,48 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+
+/*
+Sample Test Cases:
+Input 1:
+6
+1 2 3 4 5 6
+Output 1:
+Even=3, Odd=3
+
+Input 2:
+4
+2 4 6 8
+Output 2:
+Even=4, Odd=0
+
+*/
+#include <stdio.h>
+
+int main() {
+    int arr[100], n, i;
+    int evenCount = 0, oddCount = 0;
+
+    printf("Enter the number of elements in the array: ");
+    scanf("%d", &n);
+
+    printf("Enter %d integers:\n", n);
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+
+        if (arr[i] % 2 == 0)
+            evenCount++;
+        else
+            oddCount++;
+    }
+
+    printf("Number of even numbers: %d\n", evenCount);
+    printf("Number of odd numbers: %d\n", oddCount);
+
+    return 0;
+}
