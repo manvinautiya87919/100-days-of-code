3bf68567d8d0f578085ce9b35fc3764371a33823
manvinautiya87919 <manvinautiya87919@gmail.com>
2025-10-04 19:33:07 +0530

    Add files via upload

---
+++ main (77).c
@@ -0,0 +1,68 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+/*
+Sample Test Cases:
+Input 1:
+3 3
+1 2 3
+4 5 6
+7 8 1
+Output 1:
+False
+
+Input 2:
+3 3
+1 2 3
+4 5 6
+7 8 9
+Output 2:
+True
+
+*/
+#include <stdio.h>
+
+int main() {
+    int m, n;
+    printf("Enter number of rows and columns: ");
+    scanf("%d %d", &m, &n);
+
+    int matrix[m][n];
+
+    printf("Enter elements of the matrix:\n");
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    if (m != n) {
+        printf("Matrix must be square.\n");
+        return 0;
+    }
+
+    int distinct = 1;
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (matrix[i][i] == matrix[j][j]) {
+                distinct = 0;
+                break;
+            }
+        }
+        if (!distinct)
+            break;
+    }
+
+    if (distinct)
+        printf("True\n");
+    else
+        printf("False\n");
+
+    return 0;
+}
+
