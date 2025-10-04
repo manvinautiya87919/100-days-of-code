5c49600ab598ef7e9aba339ea708c7f7d6428edf
manvinautiya87919 <manvinautiya87919@gmail.com>
2025-10-04 19:52:19 +0530

    Add files via upload

---
+++ main (83).c
@@ -0,0 +1,47 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+
+/*
+Sample Test Cases:
+Input 1:
+hello
+Output 1:
+Vowels=2, Consonants=3
+
+*/
+#include <stdio.h>
+
+int main() {
+    char str[1000];
+    int vowels = 0, consonants = 0;
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
+    for (int i = 0; str[i] != '\0' && str[i] != '\n'; i++) {
+        char ch = str[i];
+
+        
+        if (ch >= 'A' && ch <= 'Z') {
+            ch = ch + 32; 
+        }
+
+        if (ch >= 'a' && ch <= 'z') {
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')
+                vowels++;
+            else
+                consonants++;
+        }
+    }
+
+    printf("Vowels=%d, Consonants=%d\n", vowels, consonants);
+
+    return 0;
+}
+
