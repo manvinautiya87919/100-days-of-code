39b7fa775e44d804678bf3ba2bd732e8667c4245
manvinautiya87919 <manvinautiya87919@gmail.com>
2025-10-04 20:02:10 +0530

    Add files via upload

---
+++ main (86).c
@@ -0,0 +1,49 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+/*
+Sample Test Cases:
+Input 1:
+madam
+Output 1:
+Palindrome
+
+Input 2:
+hello
+Output 2:
+Not palindrome
+
+*/
+#include <stdio.h>
+
+int main() {
+    char str[1000];
+    int length = 0, isPalindrome = 1;
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
+    while (str[length] != '\0' && str[length] != '\n') {
+        length++;
+    }
+
+    for (int i = 0; i < length / 2; i++) {
+        if (str[i] != str[length - i - 1]) {
+            isPalindrome = 0;
+            break;
+        }
+    }
+
+    if (isPalindrome)
+        printf("Palindrome\n");
+    else
+        printf("Not palindrome\n");
+
+    return 0;
+}
+
