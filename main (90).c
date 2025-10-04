a66b64b59bc00038cfe06853744396d43f77d686
manvinautiya87919 <manvinautiya87919@gmail.com>
2025-10-04 20:13:41 +0530

    Add files via upload

---
+++ main (90).c
@@ -0,0 +1,37 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+/*
+Sample Test Cases:
+Input 1:
+Hello
+Output 1:
+hELLO
+
+*/
+#include <stdio.h>
+
+int main() {
+    char str[1000];
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
+    for (int i = 0; str[i] != '\0' && str[i] != '\n'; i++) {
+        if (str[i] >= 'a' && str[i] <= 'z') {
+            str[i] = str[i] - 32; 
+        } 
+        else if (str[i] >= 'A' && str[i] <= 'Z') {
+            str[i] = str[i] + 32;
+        }
+    }
+
+    printf("%s\n", str);
+
+    return 0;
+}
